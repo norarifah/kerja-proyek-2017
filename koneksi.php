c51f4125308d5dcd2026be0f3f8437ccb73e7da7
latifahnh <latifahhandayani99@gmail.com>
2017-05-18 08:37:19 +0700

    aa

--- koneksi.php
+++ koneksi.php
@@ -1,3 +1,7 @@
 <?php 
-$host = "localhost"; $user = "root"; $pass = ""; $db = "latihan"; $konek = mysql_connect($host, $user, $pass) or die ('Koneksi Gagal! '); mysql_select_db($db); 
+$host = "localhost"; 
+$user = "root"; 
+$pass = ""; 
+$db = "latihan"; 
+$konek = mysql_connect($host, $user, $pass) or die ('Koneksi Gagal! '); mysql_select_db($db);
 ?>
