f3ed59bad278b1f75ace870744ec2f4ae1d5c9ca
latifahnh <latifahhandayani99@gmail.com>
2017-06-01 11:55:40 +0700

    ch

--- php-login/hasil.php
+++
@@ -1,4 +0,0 @@
-Prestasi pertama diraih oleh = 
-<?php 
-echo $_POST['id'];
-?>
--- php-login/logout.php
+++
@@ -1,6 +0,0 @@
-<?php session_start();
-unset($_SESSION['username']);
-?>
- 
-<h2>Anda telah berhasil logout..</h2>
-Silahkan klik <b>disini</b> untuk login kembali
