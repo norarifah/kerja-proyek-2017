cc304e606e360eb8f26b900bacd00071b50f737e
latifahnh <latifahhandayani99@gmail.com>
2017-06-01 23:20:06 +0700

    db_pkl

---
+++ pkl_tb.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 0.2b
+ */
+
+//
+// Database `pkl_db`
+//
+
+// `pkl_db`.`pkl_tb`
+$pkl_tb = array(
+  array('id' => '1','nama' => 'CV Inovasi Global Media','alamat' => ',Jl. Raya Plumbon 331 Banguntapan Yogyakarta','website' => ',http://www.iglomedia.com/'),
+  array('id' => '2','nama' => 'PT KAI UPT Balai Yasa Yogyakarta','alamat' => ',"Jl. Kusbini No.1 Demangan, Gondokusuman,Yogyakarta','website' => '",'),
+  array('id' => '3','nama' => 'PT. Multiintegra','alamat' => ',"Taman Berdikari Sentosa B-1D, Jl Pemuda, RT.20/RW.6, Jakarta Timur','website' => '",http://www.multiintegra.co.id/'),
+  array('id' => '4','nama' => 'PT. NISSEN CHEMITEC INDONESIA (NCI)','alamat' => ',Jl. Surya Kencana Kav. l-17 EF Desa Kutamekar Kec. Ciampel Kabupaten Karawang','website' => ','),
+  array('id' => '5','nama' => 'PT. Sarana Insan Muda Selaras (Jogja Media Net)','alamat' => ',Jl. Bhineka Tunggal Ika K-2 Sekip Yogyakarta','website' => ',http://web.jogjamedianet.com'),
+  array('id' => '6','nama' => 'PT Sarihusada Generasi Mahardhika (Sarihusada)','alamat' => ',"Jl. Kusumanegara No.173, Muja Muju, Umbulharjo, Kota Yogyakarta','website' => '",http://www.sarihusada.co.id'),
+  array('id' => '7','nama' => 'PT. Wahana Lintas Nusa Persada','alamat' => ',Perum Grahayasa A1 Jl. Raya Lempongsari sariharjo ngaglik Sleman Yogyakarta ','website' => ',http://www.wlan.net.id/'),
+  array('id' => '8','nama' => 'Pusat Sistem Informasi Universitas Islam Indonesia','alamat' => ',"Gedung Rektorat Lantai 4. Jl Kaliurang KM 14,4 Besi Jogjakarta','website' => '",'),
+  array('id' => '9','nama' => 'PT Pika Media Komunika','alamat' => ',Jl. Gejayan 51A - Yogyakarta','website' => ',http://www.pika.net.id/'),
+  array('id' => '10','nama' => 'PT. Jaringan Multimedia Indonesia','alamat' => ',Jl. Wahid Hasyim Gg. Kantil No.170C Sleman Yogyakarta','website' => ','),
+  array('id' => '11','nama' => 'PT Paragon Technology Innovation (PTI)','alamat' => ',"Jl. Swadharma Raya, Kp. Baru III No. 60 Jakarta','website' => '",http://www.pti-cosmetics.com/'),
+  array('id' => '12','nama' => 'Direktorat Sistem dan Sumber Daya Informasi (DSSDI) UGM','alamat' => ',"Jl. Pancasila No. 01 Bulaksumur, Yogyakarta','website' => '",https://dsdi.ugm.ac.id/'),
+  array('id' => '13','nama' => 'UPT.Telematika UPN Veteran Yogyakarta','alamat' => ',"Jl. SWK 104 (Lingkar Utara), Condongcatur, Yogyakarta','website' => '",http://www.upnyk.ac.id/'),
+  array('id' => '14','nama' => 'PT. AINO INDONESIA','alamat' => ',"Jl. Acasia Sekip UGM Blok L No.1, Sinduadi, Mlati, Sinduadi, Mlati,  Sleman','website' => '",https://ainosi.co.id/en/'),
+  array('id' => '15','nama' => 'PT.Murni Cahaya Pratama (CarglossPaint)','alamat' => ',"Jl. Kp. Lio Baru Km. 2, Desa Sanja, Citeureup, Sanja, Citeureup, Bogor','website' => '",http://cargloss.co.id')
+);
